d57c5fc055c3a158f730ffe6a8f227b90c4fc887
Amy Spears <amspears007@gmail.com>
2022-09-14 15:49:01 -0600

    Add friends

--- friends.rb
+++ friends.rb
@@ -1,6 +1,10 @@
 best_friend = "Matt"
+good_friend = "Diego"
 if best_friend == "Matt"
   puts "hey Matt"
+elsif good_friend == "Diego"
+  puts "hi Diego"
+
 else
-  puts "hey you're not Matt!"
-end 
+  puts "hey you're not Matt or Diego"
+end
